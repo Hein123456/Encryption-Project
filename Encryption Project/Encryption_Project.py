9a9e79c08c0d4a7b5f445da9ba6e1d0e1390b9f2
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-11 20:20:32 +0200

    In this version, we use the generate_keys() function to generate a private and public key pair, and use them to encrypt and decrypt the file. When encrypting the file, we save the private key to a PEM file, which can be used later to decrypt the file. When decrypting the file, we prompt the user to select the private key file and load it before decrypting the file.
    Note that this implementation uses RSA for public-key encryption, with a key size of 2048 bits and the OAEP padding scheme. Other encryption algorithms and padding schemes could

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -2,28 +2,29 @@ import os
 import secrets
 import tkinter as tk
 from tkinter import filedialog
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
 
-def encrypt_file(filename, key):
+def generate_keys():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def encrypt_file(filename, public_key):
     with open(filename, "rb") as file:
         data = file.read()
     
-    key_bytes = key.encode()
-    encrypted_data = bytearray()
-    for i in range(len(data)):
-        encrypted_data.append(data[i] ^ key_bytes[i % len(key_bytes)])
+    encrypted_data = public_key.encrypt(data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
         
     encrypted_filename = os.path.splitext(filename)[0] + ".enc"
     with open(encrypted_filename, "wb") as file:
         file.write(encrypted_data)
 
-def decrypt_file(filename, key):
+def decrypt_file(filename, private_key):
     with open(filename, "rb") as file:
         encrypted_data = file.read()
     
-    key_bytes = key.encode()
-    decrypted_data = bytearray()
-    for i in range(len(encrypted_data)):
-        decrypted_data.append(encrypted_data[i] ^ key_bytes[i % len(key_bytes)])
+    decrypted_data = private_key.decrypt(encrypted_data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
         
     decrypted_filename = os.path.splitext(filename)[0] + ".dec"
     with open(decrypted_filename, "wb") as file:
@@ -41,15 +42,21 @@ while True:
     if choice == "1":
         file_path = filedialog.askopenfilename()
         if file_path:
-            key = secrets.token_hex(16)
-            encrypt_file(file_path, key)
-            print(f"File '{file_path}' encrypted with key: {key}")
+            private_key, public_key = generate_keys()
+            encrypt_file(file_path, public_key)
+            private_key_filename = "private_key.pem"
+            with open(private_key_filename, "wb") as file:
+                file.write(private_key.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption()))
+            print(f"File '{file_path}' encrypted with public key and saved to '{os.path.abspath(file_path)}.enc'")
+            print(f"Private key saved to '{os.path.abspath(private_key_filename)}'")
     elif choice == "2":
         file_path = filedialog.askopenfilename()
         if file_path:
-            key = input("Enter encryption key:\n")
-            decrypt_file(file_path, key)
-            print(f"File '{file_path}' decrypted.")
+            private_key_filename = filedialog.askopenfilename(title="Select private key file")
+            with open(private_key_filename, "rb") as file:
+                private_key = serialization.load_pem_private_key(file.read(), password=None)
+            decrypt_file(file_path, private_key)
+            print(f"File '{file_path}' decrypted and saved to '{os.path.abspath(file_path)}.dec'")
     elif choice == "3":
         break
     else:
