624af37cd496eea92b94e3dd7aaa8e6652a274a8
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-11 21:32:13 +0200

    better

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -1,51 +1,72 @@
 import hashlib
 
-def Encrypt(filename, key):
-    file = open(filename, "rb")
-    data = file.read()
-    file.close()
-    
-    data = bytearray(data)
-    for index, value in enumerate(data):
-        data[index] = value ^ key
-        
-    
-    file = open("CC-" + filename, "wb")
-    file.write(data)
-    file.close()
-    
-def Decrypt(filename, key):
-    file = open(filename, "rb")
-    data = file.read()
-    file.close()
-    
-    data = bytearray(data)
-    for index, value in enumerate(data):
-        data[index] = value ^ key
-        
-    
-    file = open(filename, "wb")
-    file.write(data)
-    file.close()
-    
-def EncryptWithHash(filename, key):
-    key = hashlib.sha256(key.encode('utf-8')).digest()
-    Encrypt(filename, key)
+def encrypt_xor(data, key):
+    return bytes(a ^ b for a, b in zip(data, key))
+
+def encrypt_file_xor(filename, key):
+    with open(filename, "rb") as file:
+        data = file.read()
+    encrypted_data = encrypt_xor(data, key)
+    with open("CC-" + filename, "wb") as file:
+        file.write(encrypted_data)
+
+def decrypt_xor(data, key):
+    return bytes(a ^ b for a, b in zip(data, key))
+
+def decrypt_file_xor(filename, key):
+    with open(filename, "rb") as file:
+        data = file.read()
+    decrypted_data = decrypt_xor(data, key)
+    with open("DC-" + filename, "wb") as file:
+        file.write(decrypted_data)
+
+def encrypt_hash(data, key):
+    hash_key = hashlib.sha256(key.encode()).digest()
+    return encrypt_xor(data, hash_key)
+
+def encrypt_file_hash(filename, key):
+    with open(filename, "rb") as file:
+        data = file.read()
+    encrypted_data = encrypt_hash(data, key)
+    with open("CC-" + filename, "wb") as file:
+        file.write(encrypted_data)
+
+def decrypt_hash(data, key):
+    hash_key = hashlib.sha256(key.encode()).digest()
+    return decrypt_xor(data, hash_key)
+
+def decrypt_file_hash(filename, key):
+    with open(filename, "rb") as file:
+        data = file.read()
+    decrypted_data = decrypt_hash(data, key)
+    with open("DC-" + filename, "wb") as file:
+        file.write(decrypted_data)
 
 choice = ""
 while choice != "4":
     print("Please select your option.")
-    print("1. Encrypt File")
-    print("2. Decrypt File")
-    print("3. Encrypt File with Hashed Key")
-    print("4. Quit")
+    print("1. Encrypt File with XOR")
+    print("2. Decrypt File with XOR")
+    print("3. Encrypt File with Hash")
+    print("4. Decrypt File with Hash")
+    print("5. Quit")
     choice = input()
-    if choice in ("1", "2", "3"):
+    if choice in ["1", "2", "3", "4"]:
+        key = input("Enter a key:\n")
         filename = input("Enter filename with extension:\n")
-        key = input("Enter encryption key:\n")
     if choice == "1":
-        Encrypt(filename, int(key))
-    if choice == "2":
-        Decrypt(filename, int(key))
-    if choice == "3":
-        EncryptWithHash(filename, key)
+        encrypt_file_xor(filename, key)
+        print(f"File '{filename}' encrypted with XOR key: {key}")
+    elif choice == "2":
+        decrypt_file_xor(filename, key)
+        print(f"File '{filename}' decrypted with XOR key: {key}")
+    elif choice == "3":
+        encrypt_file_hash(filename, key)
+        print(f"File '{filename}' encrypted with hash key: {key}")
+    elif choice == "4":
+        decrypt_file_hash(filename, key)
+        print(f"File '{filename}' decrypted with hash key: {key}")
+    elif choice == "5":
+        break
+    else:
+        print("Invalid option. Please try again.")
