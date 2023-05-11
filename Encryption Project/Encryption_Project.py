bf2c2f214c12dc89cc7e485b5a431cfa42344e85
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-11 21:30:38 +0200

    hash

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -1,3 +1,5 @@
+import hashlib
+
 def Encrypt(filename, key):
     file = open(filename, "rb")
     data = file.read()
@@ -26,19 +28,24 @@ def Decrypt(filename, key):
     file.write(data)
     file.close()
     
-
+def EncryptWithHash(filename, key):
+    key = hashlib.sha256(key.encode('utf-8')).digest()
+    Encrypt(filename, key)
 
 choice = ""
-while choice != "3":
-    print("Please select you option.")
+while choice != "4":
+    print("Please select your option.")
     print("1. Encrypt File")
     print("2. Decrypt File")
-    print("3. Quit")
+    print("3. Encrypt File with Hashed Key")
+    print("4. Quit")
     choice = input()
-    if choice == "1" or choice == "2":
-        key = int(input("Enter a key as int!\n"))
+    if choice in ("1", "2", "3"):
         filename = input("Enter filename with extension:\n")
+        key = input("Enter encryption key:\n")
     if choice == "1":
-        Encrypt(filename, key)
+        Encrypt(filename, int(key))
     if choice == "2":
-        Decrypt(filename, key)
+        Decrypt(filename, int(key))
+    if choice == "3":
+        EncryptWithHash(filename, key)
