c0866da34cb1eda03d7c0bbb597636d524a639f8
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-12 11:26:31 +0200

    ui start

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -1,4 +1,5 @@
 import hashlib
+import tkinter as tk
 
 def encrypt_xor(data, key):
     return bytes(a ^ b for a, b in zip(data, key))
@@ -42,31 +43,46 @@ def decrypt_file_hash(filename, key):
     with open("DC-" + filename, "wb") as file:
         file.write(decrypted_data)
 
-choice = ""
-while choice != "4":
-    print("Please select your option.")
-    print("1. Encrypt File with XOR")
-    print("2. Decrypt File with XOR")
-    print("3. Encrypt File with Hash")
-    print("4. Decrypt File with Hash")
-    print("5. Quit")
-    choice = input()
-    if choice in ["1", "2", "3", "4"]:
-        key = input("Enter a key:\n")
-        filename = input("Enter filename with extension:\n")
-    if choice == "1":
-        encrypt_file_xor(filename, key)
-        print(f"File '{filename}' encrypted with XOR key: {key}")
-    elif choice == "2":
-        decrypt_file_xor(filename, key)
-        print(f"File '{filename}' decrypted with XOR key: {key}")
-    elif choice == "3":
-        encrypt_file_hash(filename, key)
-        print(f"File '{filename}' encrypted with hash key: {key}")
-    elif choice == "4":
-        decrypt_file_hash(filename, key)
-        print(f"File '{filename}' decrypted with hash key: {key}")
-    elif choice == "5":
-        break
-    else:
-        print("Invalid option. Please try again.")
+def user_application():
+    choice = ""
+    while choice != "4":
+        print("Please select your option.")
+        print("1. Encrypt File with XOR")
+        print("2. Decrypt File with XOR")
+        print("3. Encrypt File with Hash")
+        print("4. Decrypt File with Hash")
+        print("5. Quit")
+        choice = input()
+        if choice in ["1", "2", "3", "4"]:
+            key = input("Enter a key:\n")
+            filename = input("Enter filename with extension:\n")
+        if choice == "1":
+            encrypt_file_xor(filename, key)
+            print(f"File '{filename}' encrypted with XOR key: {key}")
+        elif choice == "2":
+            decrypt_file_xor(filename, key)
+            print(f"File '{filename}' decrypted with XOR key: {key}")
+        elif choice == "3":
+            encrypt_file_hash(filename, key)
+            print(f"File '{filename}' encrypted with hash key: {key}")
+        elif choice == "4":
+            decrypt_file_hash(filename, key)
+            print(f"File '{filename}' decrypted with hash key: {key}")
+        elif choice == "5":
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+#ui 
+
+
+root = tk.Tk()
+root.geometry("500x350")
+
+frame = tk.Frame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = tk.Label(master=frame, text="Shhhhhh! It's a secret")
+label.pack(pady=12, padx=10)
+
+root.mainloop()
