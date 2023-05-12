a772b73fde002ddb82a063b8f330750c5274199f
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-12 13:11:46 +0200

    ui first proto

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -78,18 +78,19 @@ def user_application():
 
 #ui 
 
-
 root = tk.Tk(className='Python Examples - Frame with Widgets')
 root.geometry("1000x500")
 root.configure(bg='black')
 
-frame = tk.Frame(master=root, bg='#000000')
+frame = tk.Frame(master=root, background='#000000')
 frame.pack(pady=20, padx=60)
+
 #row 0
-label2 = tk.Label(master=frame, text="Shhhhhh! It's a secret", bg='#000000', fg='#FFFFFF')
+label2 = tk.Label(master=frame, text="Shhhhhh! It's a secret", background='#000000', foreground='#FFFFFF')
 label2.grid(row=0, column=2, pady=12, padx=10)
+
 # row 1
-label = tk.Label(master=frame, text="File:", relief=tk.RIDGE, bd=2, bg='#DADADA')
+label = tk.Label(master=frame, text="File:", relief=tk.RIDGE, bd=2, background='#000000', foreground='#FFFFFF')
 label.grid(row=1, column=0, padx=5)
 
 options = ["Option 1", "Option 2", "Option 3"]
@@ -100,31 +101,45 @@ button = tk.Button(master=frame, text="Browse", bg='black', fg='white', relief=t
 button.grid(row=1, column=2, padx=5)
 
 #row 2
+label = ttk.Label(frame, text="Choose an option:", relief="groove", padding=5, foreground='#FFFFFF', background='#000000')
 
-
-
-# Create the label with a bevel
-label = ttk.Label(frame, text="Choose an option:", relief="groove", padding=5)
 label.grid(row=2, column=0, padx=5, pady=5)
 
-# Create the radio buttons
 options = ["Option 1", "Option 2", "Option 3", "Option 4"]
 var = tk.StringVar()
 var.set(options[0])
 
 for i, option in enumerate(options):
     radio_button = ttk.Radiobutton(frame, text=option, variable=var, value=option)
+
+
     radio_button.grid(row=2, column=i+1, padx=5, pady=5)
+
 #row 3
+label = ttk.Label(frame, text="Key:", relief="groove", padding=5, foreground='#FFFFFF', background='#000000')
+label.grid(row=3, column=0, padx=5, pady=5)
 
+entry1 = tk.Entry(master=frame, text='key', foreground='#FFFFFF', background='#000000')
+entry1.grid(row=3, column=2, pady=12, padx=10)
 
+#row 4
+label = ttk.Label(frame, relief="groove", padding=5, foreground='#FFFFFF', background='#000000')
+label.grid(row=4, column=0, padx=5, pady=5)
 
+options = ["Encrypt", "Decrypt"]
+var = tk.StringVar()
+var.set(options[0])
 
+for i, option in enumerate(options):
+    radio_button = ttk.Radiobutton(label, text=option, variable=var, value=option)
+    radio_button.grid(row=0, column=i, padx=5, pady=5)
 
-#entry1 = tk.Entry(master=frame, text='key')
-#entry1.grid(row=1, column=0, pady=12, padx=10)
+#row 5
+listbox1 = tk.Listbox(master=frame, fg='#FFFFFF', bg='#000000')
+listbox1.grid(row=5, column=0, padx=5, pady=5)
 
-#entry2 = tk.Entry(master=frame, text='key2')
-#entry2.grid(row=2, column=0, pady=12, padx=10)
+listbox2 = tk.Listbox(master=frame, fg='#FFFFFF', bg='#000000')
+listbox2.grid(row=5, column=1, padx=5, pady=5)
 
 root.mainloop()
+
