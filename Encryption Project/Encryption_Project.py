3fd53ffa3404ea842f3fea147a376140d04c33ce
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-13 18:15:10 +0200

    test2

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -19,7 +19,7 @@ file_path = ''
 
 
 # Set the key and initialization vector (IV)
-key = b'secretkey toets 12'
+key = b'secretkey toets 123'
 iv = b'12345678'
 
 # Create a DES cipher object with the key and IV
