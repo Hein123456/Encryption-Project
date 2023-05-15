5cf29964c7eabf36ce5d884787d0abbbced3fd23
Heinrich Enslin <37419935@mynwu.ac.za>
2023-05-15 22:16:24 +0200

    fin

--- Encryption Project/Encryption_Project.py
+++ Encryption Project/Encryption_Project.py
@@ -11,13 +11,7 @@ from Crypto.Cipher import DES
 from Crypto.Cipher import AES
 #hash
 import hashlib
-#Global
-file_flag = False
-file_path = ''
 file_name = ''
-key = b''
-iv = b''
-size = 2048
 
 #main
 def run_code():
@@ -45,200 +39,175 @@ def run_code():
     if var1.get() == "Custom":
         if var2.get() == "Encrypt":
             update_progress(1)
-            #print(entry1.get())
-            #print(rot47_encode(entry1.get()))
-            #print(hash_keyword16(rot47_encode(entry1.get())))
+     
             file_path = entry0.get()
             file_name = os.path.basename(file_path)
-            update_progress(3)
-            #print(file_path)
             AES_encrypt(file_path,hash_keyword16(rot47_encode(entry1.get())))
-            update_progress(5)
-            update_progress(7)
+            update_progress(3) 
             DES_encrypt(hash_keyword8(rot47_encode(entry1.get())),file_name)
-            #convert_file_16_8(file_name)
-            update_progress(9)
-            update_progress(10)
+            update_progress(6)
             Remove_file(file_path)
             listbox1.insert(tk.END,"File encrypted successfully")
             Remove_file('CC-')
+            update_progress(10)
 
         if var2.get() == "Decrypt":
             update_progress(1)
             file_path = entry0.get()
-            update_progress(3)
             file_name = os.path.basename(file_path)
-            update_progress(5)
-            #convert_file_8_16(file_name)
+            update_progress(2)
             DES_decrypt(hash_keyword8(rot47_encode(entry1.get())),file_path)
+            update_progress(3)
             AES_decrypt(file_name,hash_keyword16(rot47_encode(entry1.get())), file_name)
-            
-            update_progress(7)
-            #convert_file_16_8(file_name)
-            update_progress(9)
-            
-            update_progress(10)
+            update_progress(6)
             listbox2.insert(tk.END,"File decrypted successfully")
             Remove_file('DD-')
             Remove_file(file_name)
+            update_progress(10)
   
- #xor
+#XOR
 def xoe_Encrypt(filename, key):
+    # Open the input file in binary read mode
     file = open(filename, "rb")
+    # Read the data from the file
     data = file.read()
+    # Close the input file
     file.close()
-    
+
+    # Convert the data to a mutable bytearray
     data = bytearray(data)
+
+    # XOR each byte of the data with the key
     for index, value in enumerate(data):
         data[index] = value ^ key
-        
-    
+
+    # Create a new file with a prefix "CC-" and write the encrypted data to it
     file = open("CC-" + filename, "wb")
     file.write(data)
+    # Close the output file
     file.close()
-    
+
 def xor_Decrypt(filename, key):
+    # Open the encrypted file in binary read mode
     file = open(filename, "rb")
+    # Read the data from the file
     data = file.read()
+    # Close the input file
     file.close()
-    
+
+    # Convert the data to a mutable bytearray
     data = bytearray(data)
+
+    # XOR each byte of the data with the key to decrypt
     for index, value in enumerate(data):
         data[index] = value ^ key
-        
-    
+
+    # Overwrite the original file with the decrypted data
     file = open(filename, "wb")
     file.write(data)
-    file.close()   
+    # Close the output file
+    file.close()
+   
     
-
-
-#AES encrypt & decrypt #Skyf
+#AES encrypt & decrypt 
 def AES_encrypt(file_path, key):
+    # Create a new AES cipher object with the given key and EAX mode
     cipher = AES.new(key, AES.MODE_EAX)
+    
+    # Generate a random nonce (number used once)
     nonce = cipher.nonce
- 
+    
+    # Read the content of the input file
     with open(file_path, 'rb') as fin:
         plaintext = fin.read()
     
+    # Encrypt the plaintext using the cipher
     ciphertext, tag = cipher.encrypt_and_digest(plaintext)
     
+    # Write the encrypted data along with the nonce and tag to a new file
     with open('CC-', 'wb') as fout:
         fout.write(nonce + ciphertext + tag)
 
 def AES_decrypt(file_path, key, file_name):
+    # Read the encrypted data from the input file
     with open('DD-', 'rb') as fin:
         data = fin.read()
     
+    # Extract the nonce, ciphertext, and tag from the data
     nonce = data[:16]
     ciphertext = data[16:-16]
     tag = data[-16:]
     
+    # Create a new AES cipher object with the given key, EAX mode, and nonce
     cipher = AES.new(key, AES.MODE_EAX, nonce)
     
+    # Decrypt the ciphertext and verify the authenticity using the tag
     plaintext = cipher.decrypt_and_verify(ciphertext, tag)
-        
-    with open('DD-'+file_name, 'wb') as fout:
+    
+    # Write the decrypted plaintext to a new file
+    with open('DD-' + file_name, 'wb') as fout:
          fout.write(plaintext)
+
         
-
-
 def DES_encrypt(key, file_name):
+    # Create a new DES cipher object with the given key and OFB mode
     cipher = DES.new(key, DES.MODE_OFB)
+
+    # Generate a random initialization vector (IV)
     iv = cipher.iv
 
-    with open('CC-', 'rb') as input_file, open( 'CC-' + file_name, 'wb') as output_file:
-        output_file.write(iv + cipher.encrypt(input_file.read()))
-
+    # Open the input file for reading and the output file for writing
+    with open('CC-', 'rb') as input_file, open('CC-' + file_name, 'wb') as output_file:
+        # Write the IV to the output file
+        output_file.write(iv)
+        
+        # Encrypt and write the content of the input file to the output file
+        output_file.write(cipher.encrypt(input_file.read()))
 
 def DES_decrypt(key, file_path):
+    # Open the input file for reading and the output file for writing
     with open(file_path, 'rb') as input_file, open('DD-', 'wb') as output_file:
-        iv = input_file.read(8)  # Read the IV (first 8 bytes)
+        # Read the IV (first 8 bytes) from the input file
+        iv = input_file.read(8)
+
+        # Create a new DES cipher object with the given key, OFB mode, and IV
         cipher = DES.new(key, DES.MODE_OFB, iv)
-        
+
+        # Read and decrypt the ciphertext blocks from the input file
         while True:
             block = input_file.read(8)  # Read the ciphertext block (8 bytes)
             if not block:
-                break  # Reached end of file
-            
-            decrypted_block = cipher.decrypt(block)
-            output_file.write(decrypted_block)
+                break  # Reached the end of the file
 
+            decrypted_block = cipher.decrypt(block)  # Decrypt the block
+            output_file.write(decrypted_block)  # Write the decrypted block to the output file
 
 
 def text_to_int(text_input):
+    # Encode the text input to bytes using ASCII encoding
     byte_str = text_input.encode('ascii')
+
+    # Convert the byte string to an integer
     byte_int = int.from_bytes(byte_str, 'big')
+
+    # Return the integer modulo 256 plus 1
+    # Ensures the result is between 1 and 256
     return byte_int % 256 + 1
 
-def convert_file_8_16(file_name):
-    with open(file_name, 'rb') as input_file, open('decfase2.bin', 'wb') as output_file:
-        # Read the input file in blocks of 8 bytes
-        while True:
-            block = input_file.read(8)
-            if not block:
-                break  # Reached end of file
-            # Repeat the block to create a 16-byte block
-            block = block + block
-            # Write the 16-byte block to the output file
-            output_file.write(block)
-
-
-            # 8 byte to 16 byte help my asseblief
-def convert_file_16_8(file_name):
-    with open(file_name, 'rb') as input_file, open('encfase2.bin', 'wb') as output_file:
-    # Read the input file in blocks of 16 bytes
-      while True:
-        block = input_file.read(16)
-        if not block:
-            break  # Reached end of file
-         # Split the block into two 8-byte blocks
-        block1 = block[:8]
-        block2 = block[8:]
-        # Write the two blocks to the output file
-        output_file.write(block1)
-        output_file.write(block2)
-
-
-# ROT47 pass Encrypt #Skyf
+# ROT47 pass Encrypt 
 def rot47_encode(keyword):
     encoded_keyword = ""
     for char in keyword:
         ascii_code = ord(char)
         if 33 <= ascii_code <= 126:
+            # Apply the ROT47 transformation to printable ASCII characters
             encoded_ascii_code = (ascii_code - 33 + 47) % 94 + 33
             encoded_keyword += chr(encoded_ascii_code)
         else:
+            # Preserve non-printable ASCII characters
             encoded_keyword += char
+
     return encoded_keyword
-
-
-def decode_rot47(keyword):
-   # """Decode a keyword using ROT47 encryption"""
-    # Initialize an empty result string
-    result = ""
-
-    # Loop through each character in the keyword
-    for c in keyword:
-        # Get the ASCII code of the character
-        ascii_code = ord(c)
-
-        # If the character is in the range of printable ASCII characters
-        if 33 <= ascii_code <= 126:
-            # Decode the character using the ROT47 algorithm
-            decoded_char = chr(33 + ((ascii_code + 14) % 94))
-        else:
-            # Otherwise, the character is not in the range of printable ASCII characters,
-            # so just append it as is to the result string
-            decoded_char = c
-        
-        # Append the decoded character to the result string
-        result += decoded_char
-    
-    # Return the decoded result
-    return result
-
-#hash 16
 
 
 def hash_keyword16(keyword):
@@ -248,12 +217,10 @@ def hash_keyword16(keyword):
     # Update the hash object with the keyword
     sha256.update(keyword.encode())
 
-    # Get the first 13 bytes of the hash as bytes
+    # Get the first 16 bytes of the hash as bytes
     hash_bytes = sha256.digest()[:16]
 
     return hash_bytes
-
-#hash 8
 
 def hash_keyword8(keyword):
     # Create a SHA-256 hash object
@@ -266,42 +233,31 @@ def hash_keyword8(keyword):
     hash_bytes = sha256.digest()[:8]
 
     return hash_bytes
-# locked file 1 pass 2 file #Jaap
 
-
-
-
-#source file delete #Skyf
 def Remove_file(file_path):
+    # Check if the file exists
     if os.path.exists(file_path):
+        # Remove the file
         os.remove(file_path)
-        listbox1.insert(tk.END,"File removed successfully")
-    else:
-        listbox1.insert(tk.END,"File could not be removed")
 
-# integration #Skyf en Jaap
-
-# get file bytes
-def get_file_bytes(file_path):
-    with open(file_path, 'rb') as input_file:
-            file_bytes = input_file.read()
-            return file_bytes
-#gui #Jaap
 def get_file():
-    #file_flag = True
+    # Open a file dialog to select a file
     file_path = filedialog.askopenfilename()
+
+    # Get the file type from the file path
     file_type = os.path.splitext(file_path)[1]
-   
 
+    # Clear the entry field
     entry0.delete(0, tk.END)
+
+    # Insert the selected file path into the entry field
     entry0.insert(0, file_path)
+
     
-
+#====================GUI========================
 root = tk.Tk(className='Encryption Project')
-
 root.geometry("500x450")
 root.configure(background='#000000')
-
 frame = tk.Frame(master=root, background='#000000', relief=tk.GROOVE, bd=1)
 frame.pack(pady=20, padx=60)
 
@@ -321,10 +277,8 @@ tk.Button(master=frame, text="Browse", bg='black', fg='#20C20E', **groove_style,
 
 # row 2
 tk.Label(frame, text="Choose an option:", **groove_style, **label_style).grid(row=2, column=0, padx=10, sticky="W")
-
 options2 = ["Default", "Custom"]
 var1 = tk.StringVar(value=options2[0])
-
 for i, option in enumerate(options2):
    rad1= Radiobutton(
         frame, text=option, value=option, bg='#000000', fg='#20C20E',
@@ -339,15 +293,14 @@ entry1.grid(row=3, column=1, padx=10, columnspan=2, sticky="W")
 
 # row 4
 tk.Label(frame, text="Mode:", **groove_style, **label_style).grid(row=4, padx=10, sticky="W")
-
 options3 = ["Encrypt", "Decrypt"]
 var2 = tk.StringVar(value=options3[0])
-
 for i, option in enumerate(options3):
     rad2 = Radiobutton(
         frame, text=option, value=option, bg='#000000', fg='#20C20E',
         highlightbackground='black', highlightcolor='black', selectcolor='black', variable=var2
     ).grid(row=4, column=i+1, padx=10, sticky="W")
+
 #row 5
 listbox1 = tk.Listbox(master=frame, fg='#20C20E', bg='#000000',relief=tk.GROOVE, bd=1)
 listbox1.grid(row=5, column=0,padx = 10,columnspan=2,sticky="W" )
@@ -359,6 +312,7 @@ listbox2.grid(row=5, column=2 ,padx = 10,sticky="E")
 def update_progress(progress):
     progress_width = progress * frame.winfo_width() // 10
     progressbar.coords("progress", 0, 0, progress_width, 20)
+
 def set_progress(num):
     if num < 1:
         num = 1
@@ -366,15 +320,10 @@ def set_progress(num):
         num = 10
     progress_percent = (num - 1) * 10  # calculate progress percentage
     update_progress(progress_percent / 100)  # update progress bar
+
 progressbar = tk.Canvas(frame,height=20, bg='#000000', highlightthickness=0, relief=tk.GROOVE, bd=1)
 progressbar.grid(row=6, column=0,columnspan=3 , sticky="W")
-
 progressbar.create_rectangle(0, 0, 0, 20, fill='#20C20E', width=0, tags="progress")
-
-
-# Cal
-
-
 
 root.mainloop()
 
